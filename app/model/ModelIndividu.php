496a31cc5acbcb6062a790cc3debfb544c929d4f
Nathan Vilmen <nathanvilmen@hotmail.fr>
2022-06-06 13:29:16 +0200

    Modifications page individu, lien en cours de développement.

--- app/model/ModelIndividu.php
+++ app/model/ModelIndividu.php
@@ -238,37 +238,142 @@ class ModelIndividu
             $nom_individu = $individu[0];
             $prenom_individu = $individu[1];
 
+            //On cherche l'id de l'individu
+            $query0="select id, sexe from individu where nom=:nom and prenom=:prenom";
+            $statement = $database->prepare($query0);
+            $statement->execute([
+                'nom'=> $nom_individu,
+                'prenom'=> $prenom_individu
+            ]);
+            $result0=$statement->fetch();
+            $iid=$result0[0];
+            $sexe=$result0[1];
+
             //On cherche l'année de naissance, le lieu de naissance, l'année de décès et le lieu de décès
             $famille_id = ModelFamille::getIdFamille($famille);
 
             /* Infos naissance */
-            $query1="select event_date, event_lieu from evenement where famille_id=:famille_id and event_type='NAISSANCE' and iid=(select id from individu where nom=:nom and prenom=:prenom)";
+            $query1="select event_date, event_lieu from evenement where famille_id=:famille_id and event_type='NAISSANCE' and iid=:iid";
             $statement = $database->prepare($query1);
             $statement->execute([
                 'famille_id' => $famille_id,
-                'nom'=> $nom_individu,
-                'prenom'=> $prenom_individu
-
+                'iid'=> $iid
             ]);
             $result1=$statement->fetch();
             $date_naissance=$result1[0];
             $lieu_naissance=$result1[1];
 
             /* Infos décès */
-            $query2="select event_date, event_lieu from evenement where famille_id=:famille_id and event_type='DECES' and iid=(select id from individu where nom=:nom and prenom=:prenom)";
+            $query2="select event_date, event_lieu from evenement where famille_id=:famille_id and event_type='DECES' and iid=:iid";
             $statement = $database->prepare($query2);
+            $statement->execute([
+                'famille_id' => $famille_id,
+                'iid'=> $iid
+            ]);
+            $result2=$statement->fetch();
+            $date_deces=$result2[0];
+            $lieu_deces=$result2[1];
+
+            // Recherche des parents
+            /* Id des parents */
+            $query3 = "select pere, mere from individu where famille_id=:famille_id and nom=:nom and prenom=:prenom";
+            $statement = $database->prepare($query3);
             $statement->execute([
                 'famille_id' => $famille_id,
                 'nom'=> $nom_individu,
                 'prenom'=> $prenom_individu
 
             ]);
-            $result2=$statement->fetch();
-            $date_deces=$result2[0];
-            $lieu_deces=$result2[1];
+            $result3=$statement->fetch();
+            $id_pere=$result3[0];
+            $id_mere=$result3[1];
 
+            /* Noms des parents */
+            $query4 = "select nom, prenom from individu where famille_id=:famille_id and id=:id_pere";
+            $statement = $database->prepare($query4);
+            $statement->execute([
+                'famille_id'=>$famille_id,
+                'id_pere'=> $id_pere
+            ]);
+            $result4=$statement->fetch();
+            $nom_pere=$result4[0];
+            $prenom_pere=$result4[1];
 
-            return array($nom_individu, $prenom_individu, $date_naissance, $lieu_naissance, $date_deces, $lieu_deces);
+            $query5 = "select nom, prenom from individu where famille_id=:famille_id and id=:id_mere";
+            $statement = $database->prepare($query5);
+            $statement->execute([
+                'famille_id'=>$famille_id,
+                'id_mere'=> $id_mere
+            ]);
+            $result5=$statement->fetch();
+            $nom_mere=$result5[0];
+            $prenom_mere=$result5[1];
+
+            // Recherche des unions et des enfants issus de ces unions
+
+            // Test si femmme ou Homme. La BDD des lien est organisée pour que l'homme soit en premier (iid1) et
+            // la femme en deuxième (iid2). Si c'est un homme, je cherche les iid2, si c'est une femme, je cherche des iid1
+            if ($sexe = 'H'){
+                /* ids des unions */
+                $query6 = "select iid2 from lien where iid1=:iid1 and famille_id=:famille_id and (lien_type='MARIAGE' or lien_type='COUPLE' or lien_type='PACS')";
+                $statement = $database->prepare($query6);
+                $statement->execute([
+                    'famille_id'=>$famille_id,
+                    'iid1'=> $iid
+                ]);
+                $result6=$statement->fetchAll();
+                $id_mariees=$result6;
+            } else if ($sexe = 'F'){
+                $query7 = "select iid1 from lien where iid2=:iid2 and famille_id=:famille_id and (lien_type='MARIAGE' or lien_type='COUPLE' or lien_type='PACS')";
+                $statement = $database->prepare($query7);
+                $statement->execute([
+                    'famille_id'=>$famille_id,
+                    'iid2'=> $iid
+                ]);
+                $result7=$statement->fetchAll();
+                $id_mariees=$result7;
+            }
+            //print_r($id_mariees);
+
+            //Alors si $id_unions est NULL, cela veut bien dire qu'il n'y a pas d'union.
+
+            /* Noms des mariées */
+            $noms_mariees=array();
+            $prenoms_mariees=array();
+
+            $enfants_mariees = array();  //Tableau des enfants pour chaque union
+
+            $noms_enfants=array();
+            $prenoms_enfants=array();
+
+            //id_mariees c'est plusieurs iid1 ou iid2
+            foreach($id_mariees as $id){
+                //print_r($id);
+                //On cherche le nom et le prénom du conjoint
+                $query8 = "select nom, prenom from individu where id=:id and famille_id=:famille_id";
+                $statement = $database->prepare($query8);
+                $statement->execute([
+                    'id'=> $id[0],
+                    'famille_id'=>$famille_id
+                ]);
+                $result8=$statement->fetch();
+                $noms_mariees[]=$result8[0];
+                $prenoms_mariees[]=$result8[1];
+                //print_r($noms_mariees);
+                //print_r($prenoms_mariees);
+
+                // Recherche des enfants de chaque union
+                $query9="select nom, prenom from individu where famille_id=:famille_id and (pere=:id1 and mere=:id2) or (pere=:id2 and mere=:id1)";
+                $statement = $database->prepare($query9);
+                $statement->execute([
+                    'famille_id'=>$famille_id,
+                    'id1'=> $iid,
+                    'id2'=> $id[0]
+                ]);
+                $enfants_mariees[]=$statement->fetchAll();
+                //print_r($enfants_mariees);
+            }
+            return array($nom_individu, $prenom_individu, $date_naissance, $lieu_naissance, $date_deces, $lieu_deces, $nom_pere, $prenom_pere, $nom_mere, $prenom_mere, $noms_mariees, $prenoms_mariees, $enfants_mariees);
         }
         catch (PDOException $e) {
             printf("%s - %s<p/>\n", $e->getCode(), $e->getMessage());
--- app/view/individu/individuViewSelected.php
+++ app/view/individu/individuViewSelected.php
@@ -16,9 +16,22 @@ require($root . '/app/view/fragment/fragmentGenealogieHeader.html');
         echo "<li>Décès le ".$results[4]." à ".$results[5]."</li></ul><br/>";
 
         echo "<h1>Parents</h1>";
-        echo "<ul><li>Père : </li>";
-        echo "<li>Mère : </li></ul>";
+        echo "<ul><li>Père : <a href='router.php?action=individuSelected&individu='.$results[6].$results[7]>".$results[6]." ".$results[7]."</a></li>";
+        echo "<li>Mère : <a href='router.php?action=individuSelected&target='>".$results[8]." ".$results[9]."</a></li></ul>";
 
+        echo "<h1>Unions et enfants</h1>";
+        $i=0;
+        foreach ($results[10] as $nom_mariee){
+            echo "<ul><li>Union avec <a href='router.php?action=individuSelected&individu='.$nom_mariee.$results[11][$i].></a></li>";
+            echo "<ol>";
+            foreach ($results[12][$i] as $enfant){
+
+                echo "<li>Enfant ".$enfant[0]." ".$enfant[1]."</li>";
+            }
+            echo "</ol>";
+            echo "</ul>";
+            $i++;
+        }
     }
     else{
         echo "<h2>Problème dans la sélection de l'individu</h2>";
